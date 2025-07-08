fe8bb0a6e62cd500b86ac7c2d6b4a2378c25ef29
Grant Morfitt <grant.morfitt@gmail.com>
2025-07-08 16:03:53 -0400

    Spatial Fog Connection Manager

--- FullRecorder.py
+++ FullRecorder.py
@@ -27,7 +27,7 @@ class AppGUI:
         tk.Label(root, text="Filename:").grid(row=0, column=0, padx=(10, 2), pady=5, sticky='e')
         self.filename_entry = tk.Entry(root, width=40)
         self.filename_entry.grid(row=0, column=1, columnspan=3, padx=(2, 10), pady=5, sticky='w')
-        self.filename_entry.insert(0, f"{datetime.now().strftime('%Y-%m-%d_%H%M%S')}.csv")
+        self.filename_entry.insert(0, f"Write filename here")
 
         # Log box (scrolled text)
         self.log_box = scrolledtext.ScrolledText(root, width=75, height=20, state='disabled')
@@ -37,7 +37,10 @@ class AppGUI:
         tk.Button(root, text="Start", command=self.start_acquisition, width=15).grid(row=1, column=0, padx=10, pady=10)
         tk.Button(root, text="Stop", command=self.stop_acquisition, width=15).grid(row=1, column=1, padx=10, pady=10)
         tk.Button(root, text="Quit", command= lambda: root.destroy(), width=15).grid(row=1, column=3, padx=10, pady=10)
-
+        
+        self.led = tk.Canvas(root, width=25, height=25, highlightthickness=0)
+        self.led.grid(row=0, column=3, padx=10, pady=10)
+        self.led_circle = self.led.create_oval(2, 2, 22, 22, fill="gray")
         
     def log(self, msg):
         timestamp = datetime.now().strftime('%H:%M:%S')
@@ -48,13 +51,15 @@ class AppGUI:
 
     def get_filename(self):
         name = self.filename_entry.get().strip()
+        now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        
         if not name:
-            return "data"
-        else: return name
+            return f"{now}_data"
+        else: return f"{now}_{name}"
         
     def start_acquisition(self):
-        
-        DIfilename = "ControlPositions_" + self.get_filename()
+        self.led.itemconfigure(self.led_circle, fill="green")
+        DIfilename = "ControlPos_" + self.get_filename()
         if not DIfilename.lower().endswith(".csv"):
             DIfilename += ".csv"
         threading.Thread(target=self.data_recorder1.start_acquisition, args = (DIfilename,), daemon=True).start()
@@ -66,19 +71,26 @@ class AppGUI:
         #this will need to pass filenames over once I get it set up properly
         print("lorem")
     def stop_acquisition(self):
+        self.led.itemconfigure(self.led_circle, fill="gray")
         self.data_recorder1.stop_acquisition()
         self.data_recorder2.stop_acquisition()
     
 
 class DIDataRecorder:
     def __init__(self, log_callback):
+        self. heartbeat_interval = 5  # seconds
         self.ser = serial.Serial()
         self.acquiring = False
         self.stop_event = threading.Event()
         self.range_table = []
         self.log = log_callback
 
-        self.slist = [0x0A00, 0x0B01, 0x1702]  # Customize as needed
+        self.slist = [0x0900, 0x0901, 0x0902, 0X0903]  # Customize as needed
+        #self.slist = [0xB00]
+        #self.slist = [2304,2305, 2306, 2307]
+        self.colNames = ["Pitch", "Roll", "Collective", "Pedal"]
+        #Change slist tuple to vary analog channel configuration.
+        
         self.analog_ranges = [.5, 0.25, 0.1, .05, .025, .01, 0, 0, 50, 25, 10, 5, 2.5, 1, 0, 0]
 
     def send_cmd(self, cmd):
@@ -89,9 +101,6 @@ class DIDataRecorder:
                 while self.ser.in_waiting:
                     try:
                         line = self.ser.readline().decode().strip()
-                        if line:
-                            self.log(f"Device: {line}")
-                            break
                     except:
                         continue
         except Exception as e:
@@ -105,11 +114,13 @@ class DIDataRecorder:
                 self.ser.baudrate = 115200
                 self.ser.timeout = 0
                 try:
+                    if self.ser.is_open:
+                        self.ser.close()
                     self.ser.open()
                     self.log(f"Connected to {port.device}")
                     return True
                 except Exception as e:
-                    self.log(f"Failed to open port: {e}")
+                    self.log(f"Failed to open DI-2008 port: {e}")
                     return False
         self.log("No DI-2008 device found.")
         return False
@@ -123,24 +134,24 @@ class DIDataRecorder:
             else:
                 self.range_table.append(0)
 
-    def initialize(self, on_fail_quit):
+    def initialize(self):
         if self.discover_device():
             self.send_cmd("stop")
-            self.send_cmd("ps 0")
-            self.send_cmd("dec 20")
-            self.send_cmd("srate 1")
+            self.send_cmd("ps 1")
+            self.send_cmd("dec 1")
+            self.send_cmd("srate 10")
+
             self.config_scan_list()
-            self.log("DI-2008 Device initialized and ready.")
+            self.log("DI-2008 Device initialized")
         else:
             messagebox.showerror("Error", "DI-2008 not found.")
-            on_fail_quit()
 
     def start_acquisition(self, filename):
         if self.acquiring:
             self.log("Already acquiring.")
             return
         if not self.ser.is_open:
-            self.log("Device not connected.")
+            self.log("Spatial FOG not connected.")
             return
         self.acquiring = True
         self.stop_event.clear()
@@ -152,21 +163,22 @@ class DIDataRecorder:
             self.log("Already stopped.")
             return
         self.acquiring = False
-        self.stop_event.set()
         self.send_cmd("stop")
+        self.stop_event.set()
         self.log("Stop pressed. Acquisition stopped.")
         self.ser.close()
-        
-    def reset_counter(self):
-        self.send_cmd("reset 1")
-        self.log("Counter reset sent.")
+    
 
     def acquire_data(self, filename):
         try:
             self.send_cmd("start")
             with open(filename, "w", newline="") as file:
                 writer = csv.writer(file)
-                writer.writerow(["Time"] + [f"Ch{i}" for i in range(len(self.slist))])
+                writer.writerow(["Time"] + [f"{v}" for i,v in enumerate(self.colNames)])
+                
+                last_heartbeat = time.time()
+
+           
                 while self.acquiring and not self.stop_event.is_set():
                     if self.ser.in_waiting >= 2 * len(self.slist):
                         row = [datetime.now().strftime('%H:%M:%S')]
@@ -175,17 +187,21 @@ class DIDataRecorder:
                             val = int.from_bytes(raw, 'little', signed=True)
                             row.append(val)
                         writer.writerow(row)
-                        self.log(f"Control Data: {', '.join(map(str, row))}")
-                    time.sleep(0.1)
+                        #self.log(f"Control Data: {', '.join(map(str, row))}")
+                    
+                    if time.time() - last_heartbeat >= self.heartbeat_interval:
+                       self.log(f"{row[0]} DI-2008: Heartbeat")
+                       last_heartbeat = time.time()
+                       
             self.log(f"Data written to {filename}")
         except Exception as e:
             self.log(f"Control Record Error: {e}")
-        finally:
-            self.acquiring = False
-            self.send_cmd("stop")
+            
+
 
 class SpatialFogDataRecorder:
     def __init__(self, log_callback):
+        self.heartbeat_interval = 5
         self.ser = serial.Serial()
         self.acquiring = False
         self.stop_event = threading.Event()
@@ -194,12 +210,54 @@ class SpatialFogDataRecorder:
         self.spatial = None
         self.comport = 'COM5'
         self.baudrate = 1250000
-            
-    def stop_acquisition(self):
-        self.stop_event.set()
-        self.acquiring = False
-        self.ser.close()
         
+        
+    
+    def initialize(self):
+        self.log("Checking Spatial FOG device...")
+   
+
+        try:
+            test_spatial = spatial_device.Spatial(self.comport, self.baudrate)
+            test_spatial.start()
+    
+            if not test_spatial.is_open:
+                self.log("Serial port not open.")
+                messagebox.showerror("Device Error", "Spatial FOG device not found.")
+                return False
+    
+            self.log("Serial port is open. Waiting for incoming ANPP packet...")
+    
+            # Flush any old data
+            test_spatial.flush()
+            time.sleep(0.5)
+    
+            # Read a few bytes from the buffer
+            timeout = time.time() + 2  # 2 second timeout
+            found_bytes = False
+    
+            while time.time() < timeout:
+                if test_spatial.in_waiting() > 0:
+                    found_bytes = True
+                    break
+                time.sleep(0.1)
+    
+            if found_bytes:
+                self.log("Spatial FOG is powered on and sending data.")
+                test_spatial.close()
+                return True
+            else:
+                self.log("No response from Spatial FOG (in_waiting = 0).")
+                messagebox.showerror("Device Error", "Spatial FOG is connected but not responding. Check if it's powered on.")
+                return False
+    
+        except Exception as e:
+            self.log(f"Spatial device check error: {e}")
+            messagebox.showerror("Device Error", f"Error initializing Spatial FOG:\n{e}. \rCheck if device is connected!")
+            return False
+            
+        
+
     def start_acquisition(self, filename):
         
         spatial = spatial_device.Spatial(self.comport, self.baudrate)
@@ -208,12 +266,12 @@ class SpatialFogDataRecorder:
         if spatial.is_open:
           print(f"Connected to port:{spatial.port} with baud:{spatial.baud}")
           spatial.flush()
-    
+          
           # Creates log file for received binary data from device
           now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
           logFile = open(f"{filename}.anpp", 'xb')
     
-          an_packet = ANPacket()
+          #an_packet = ANPacket()
     
           # Sets sensor ranges
           spatial.set_sensor_ranges(True,
@@ -222,6 +280,9 @@ class SpatialFogDataRecorder:
                                     spatial_device.MagnetometerRange.magnetometer_range_8g)
     
           spatial.get_device_and_configuration_information()
+          
+          last_heartbeat = time.time()
+          
           self.acquiring = True
           try:
               while spatial.is_open:
@@ -233,30 +294,88 @@ class SpatialFogDataRecorder:
             
                           # Record in log file the raw binary of ANPP packets
                           logFile.write(data_bytes)
-            
-                          self.log(datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + " Spatial Record Bytes")
+                          if time.time() - last_heartbeat >= self.heartbeat_interval:
+                             self.log(f"{datetime.now().strftime('%H:%M:%S')} Spatial Fog: Heartbeat")
+                             last_heartbeat = time.time()
+                             
+                             
+                          #self.log(datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + " Spatial Record Bytes")
                       else:
                           logFile.close()
                           self.acquiring = False
-                          self.log(f"Data written to {filename}")
+                          self.log(f"Spatial Data written to {filename}")
                           break;
           except Exception as e:
               self.log(f"Spatial FOG Error: {e}")       
+              
+          
+              
+        elif not spatial.is_open():
+            self.log("WARNING: Spatial Fog not detected!") 
+            
+        
+    
+    def stop_acquisition(self):
+        if not self.acquiring:
+            self.log("Already stopped.")
+            return
+        self.acquiring = False
+        self.stop_event.set()
 
+        self.log("Stop pressed. Spatial Acquisition stopped.")
+        self.ser.close()
 
+def MasterCloseSerial(instanceOne, instanceTwo):
+    """
+    Ensure all serial connections are closed in the event that the user clicks the close button instead of quit,
+    or program has an unexpected error
 
+    Returns
+    -------
+    None.
+
+    """
+    instanceOne.ser.close()
+    instanceTwo.ser.close()
+    root.destroy()
+    sys.exit()
 
 if __name__ == "__main__":
+    
     root = tk.Tk()
+
+    
     DIrecorder = DIDataRecorder(log_callback=lambda msg: gui.log(msg))  # placeholder, will reassign after gui created
     SpatialRecorder = SpatialFogDataRecorder(log_callback=lambda msg: gui.log(msg))
-    
     gui = AppGUI(root, DIrecorder, SpatialRecorder)
     
+
+    
+
     DIrecorder.log = gui.log  # assign log callback after GUI init.
     SpatialRecorder.log = gui.log
-    DIrecorder.initialize(root.quit)
         
     # Start discovery on launch in a thread
     #threading.Thread(target=DIrecorder.initialize, args=(root.quit,), daemon=True).start()
+    SpatialRecorder.initialize()
+    DIrecorder.initialize()
+
+    root.protocol("WM_DELETE_WINDOW", lambda: MasterCloseSerial(SpatialRecorder, DIrecorder))
     root.mainloop()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
